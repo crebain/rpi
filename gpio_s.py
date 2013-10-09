ae2ce86e4cdcb26a0ca8391c1387ee16c5a877af
Laurynas Ruðkys <bilbas@gmail.com>
2013-10-09 21:39:00 +0300

    Implemented shell command execution functionality

--- gpio_s.py
+++ gpio_s.py
@@ -5,24 +5,52 @@ import select
 import errno
 import os
 import subprocess
-
-def print_help(argv):
-    print ('%(cmd)s -i <inputfile> -o <outputfile>' % { "cmd": argv[0] })
+from datetime import datetime
+from datetime import timedelta
+import logging
 
 def main(argv):
     parser = argparse.ArgumentParser (description="poll(2) for specified INPUT and execute CMD when it changes")
     parser.add_argument ('command', metavar='CMD', help='Command that wil be performed when INPUT changes')
-    parser.add_argument ('arguments', nargs='*', metavar='ARGS', help='Optional arguments for CMD. %s will be replaced by value read from INPUT. Use %% to display a single %.')
-    parser.add_argument ('-i', '--input', required=True, metavar='INPUT', type=argparse.FileType ('r'), help='The file handle to poll(2)')
-    args = parser.parse_args()
+    parser.add_argument ('arguments', nargs='*', metavar='ARGS', help='Optional arguments for CMD.')
+    parser.add_argument ('-i', '--input', required=True, metavar='INPUT', type=argparse.FileType ('r'), help='The file to poll(2)')
+    parser.add_argument ('-w', '--while', required=False, metavar='VALUE', default='1', help="Keeps CMD running while content of INPUT equals to VALUE. Default '1'")
+    parser.add_argument ('-t', '--timeout', required=False, metavar='TIMEOUT', type=int, default=0, help='Timeout when CMD will be terminated, in seconds. Defaults to 0, i.e. CMD will be terminated when VALUE does not match specified.')
+    parser.add_argument ('-v', '--verbosity', required=False, metavar='LEVEL', default=logging.getLevelName (logging.WARNING), help='Verbosity level. See http://docs.python.org/2/library/logging.html for possible values.')
 
-    print ('Input file is %s' % args.input)
-    command = " ".join ([args.command] + args.arguments)
-    print ("Command to execute: '%(command)s'" % { 'command': command })
+    args = vars (parser.parse_args())
+
+    input = args['input']
+    command = " ".join ([args['command']] + args['arguments'])
+    timeout = timedelta (seconds=args['timeout'])
+    whileValue = args['while']
+
+    logger = logging.getLogger(__package__)
+    logger.setLevel(args['verbosity'])
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(args['verbosity'])
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    logger.debug ('Input file is %s' % input)
+    logger.debug ("Command to execute: '%(command)s'" % { 'command': command })
+
+    process = None
+    start = datetime.now()
+
+    poller = select.poll()
+    poller.register (input, select.POLLPRI)
 
     while (True):
-        poller = select.poll()
-        poller.register (args.input, select.POLLPRI)
         list = poller.poll ()
 
         for entry in list:
@@ -30,12 +58,31 @@ def main(argv):
             events = entry[1]
 
             if events & select.POLLPRI:
-                args.input.seek (0)
-                value = args.input.read (1)
-                subprocess.call (command % value, shell=True)
+                logger.debug ('poll: fd %d, events %d' % (fd, events))
+                input.seek (0)
+                value = input.read ().strip ()
+                logger.debug ("read value '%s'" % value)
+
+                result = None
+                if None != process:
+                    result = process.poll()
+                    logger.debug ('previous process finished with code %d' % result)
+
+                if value == whileValue:
+                    start = datetime.now()
+
+                    if None == process or result != None:
+                        logger.debug ("starting process '%s'" % command)
+                        # Start a new process if old one already exited
+                        process = subprocess.Popen (command, shell=True)
+                else:
+                    if datetime.now() > start+timeout and None != process and None == process.poll():
+                        process.terminate ()
 
 
-    os.close (args.input)
+    os.close (input)
+    if None != process and None != process.poll():
+        process.terminate ()
 
 
 
